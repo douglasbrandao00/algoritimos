f9df83cbdfc7452425a5fb254f7cd7bde0f5d850
douglas.brandao <douglas.branado@softdesign.com>
2022-05-10 17:01:06 -0300

    setup project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const root = path.resolve(__dirname);
+module.exports = {
+  rootDir: root,
+  testMatch: ['<rootDir>/**/__tests__/*.spec.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    'App/(.*)': '<rootDir>/src/$1',
+    'Test/(.*)': '<rootDir>/test/$1',
+  },
+};
